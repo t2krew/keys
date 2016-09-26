6115ab8d0db6ee7f2dd91db09d286d74b875da5f
Krew <wanhao.huang@outlook.com>
2016-09-27 02:14:09 +0800

    获取对象中的keys

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 'use strict';
 
-/*
- * get the object keys
- * @param obj {Object} 
- * 
+/**
+ * 获取对象中的key
+ * @param  {Object} obj 源对象
+ * @return {Array} keys 源对象中的key集合
  */
-
 exports = module.exports = function(obj){
-	if(!obj){
-		throw Error('miss the argument');
-	}
-	if(Object.keys){
-		return Object.keys(obj)
-	}else{
-		let a = [];
-		for(a[a.length] in obj);
-		return a;
-	}
+  if(!obj){
+    throw Error('缺少参数obj!');
+  }
+  //判断是否支持原生Object.keys方法
+  //可用则使用原生方法获取keys
+  if(Object.keys){
+    return Object.keys(obj)
+  }else{
+    //如果不支持Object.keys方法
+    //则使用polyfill
+    let keys = [];
+    for(keys[keys.length] in obj);
+    return keys;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -4,32 +4,32 @@ let assert = require('assert');
 let keys = require('../');
 
 let obj = {
-	name : 'krew',
-	age : 25,
-	sex : 'male',
-	hobby : 'basketball'
+  name : 'krew',
+  age : 25,
+  sex : 'male',
+  hobby : 'basketball'
 }
 
 describe('keys test',function(){
-	it('should throw an Error',function(){
-		assert.throws(function(){
-			keys()
-		},
-		function(err){
-			if(err.message == 'miss the argument'){
-				return true;
-			}
-		})
-	});
+  it('should throw an Error',function(){
+    assert.throws(function(){
+      keys()
+    },
+    function(err){
+      if(err.message == 'miss the argument'){
+        return true;
+      }
+    })
+  });
 
-	it('should return a array of object keys',function(){
-		assert.doesNotThrow(function(){
-			keys(obj)
-		},
-		function(d){
-			if((typeof d == 'array') && d.length == 4){
-				return true;
-			}
-		})
-	})
+  it('should return a array of object keys',function(){
+    assert.doesNotThrow(function(){
+      keys(obj)
+    },
+    function(d){
+      if((typeof d == 'array') && d.length == 4){
+        return true;
+      }
+    })
+  })
 })
